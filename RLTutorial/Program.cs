071598e4559fd091fdff089062f9fffe65678ff5
jwoodger <jwoodger@github.com>
2019-06-16 00:04:53 -0400

    Initial commit.

---
+++ RLTutorial/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RLTutorial {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Welcome to the game!");
+        }
+    }
+}
