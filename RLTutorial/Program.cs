2e584c1ed057f5e01d990dedce57be9a3258774a
jwoodger <jwoodger@github.com>
2019-06-16 09:40:15 -0400

    Added documentation.

--- RLTutorial/Program.cs
+++ RLTutorial/Program.cs
@@ -4,11 +4,25 @@ using SadConsole;
 
 namespace RLTutorial {
 
+    /// <summary>
+    ///   Contains the main application code.
+    /// </summary>
     class Program {
 
+        /// <summary>
+        ///   The width of the console, in characters.
+        /// </summary>
         const int ConsoleWidth = 80;
+
+        /// <summary>
+        ///   The height of the console, in characters.
+        /// </summary>
         const int ConsoleHeight = 25;
 
+        /// <summary>
+        ///   The main entry point to the program.
+        /// </summary>
+        /// <param name="args">Command-line arguments (unused).</param>
         static void Main(string[] args) {
             SadConsole.Game.Create(ConsoleWidth, ConsoleHeight);
             SadConsole.Game.OnInitialize = Initialize;
@@ -17,6 +31,9 @@ namespace RLTutorial {
             SadConsole.Game.Instance.Dispose();
         }
 
+        /// <summary>
+        ///   Callback for SadConsole. Sets up the console and other components.
+        /// </summary>
         static void Initialize() {
             var console = new Console(ConsoleWidth, ConsoleHeight);
             console.FillWithRandomGarbage();
