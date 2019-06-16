2144bcf5884539b317561719fa95954a209521dd
jwoodger <jwoodger@github.com>
2019-06-16 09:37:42 -0400

    Set up console library. Application opens a window and shows a message.

--- RLTutorial/Program.cs
+++ RLTutorial/Program.cs
@@ -1,12 +1,29 @@
-using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SadConsole;
 
 namespace RLTutorial {
 
     class Program {
 
+        const int ConsoleWidth = 80;
+        const int ConsoleHeight = 25;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello!");
-            Console.WriteLine("Welcome to the game!");
+            SadConsole.Game.Create(ConsoleWidth, ConsoleHeight);
+            SadConsole.Game.OnInitialize = Initialize;
+
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        static void Initialize() {
+            var console = new Console(ConsoleWidth, ConsoleHeight);
+            console.FillWithRandomGarbage();
+            console.Fill(new Rectangle(3, 3, 22, 3), Color.White, Color.Black, ' ', SpriteEffects.None);
+            console.Print(4, 4, "Welcome to the game!");
+
+            SadConsole.Global.CurrentScreen = console;
         }
     }
 }
